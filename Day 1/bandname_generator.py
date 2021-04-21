d823b097754de17284bd49406260d224092a7f12
Eliud Kagema <eliud.k.maina@gmail.com>
2021-04-21 07:46:54 +0300

    string manupulation refressher

---
+++ Day 1/bandname_generator.py
@@ -0,0 +1,14 @@
+# String manipulation program
+
+#1. Create a greeting for your program.
+print("Welcome to the band name generator")
+
+#2. Ask the user to input the city they grew in
+city = input('Which city did you grow up in?\n')
+
+#3. Ask the user for the name of a pet
+pet = input('Whats the name of a pet?\n')
+
+#4. Combine the name of their city and pet name to generate a bandname
+print('Your bandname could be ' + city + " " + pet)
+
